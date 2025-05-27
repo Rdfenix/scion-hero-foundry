dc49284b70bf4c845081aace2dc8b870c018879c
Rudnei Oliveira <rhoodsk8erboy@gmail.com>
2025-05-27 09:06:40 +0100

    create: creating structure from scion hero foundry

---
+++ module/actor-sheet.js
@@ -0,0 +1,43 @@
+export class ScionHeroActorSheet extends ActorSheet {
+  prepareData() {
+    super.prepareData();
+  }
+
+  static get defaultOptions() {
+    return foundry.utils.mergeObject(super.defaultOptions, {
+      classes: ["scion-hero", "sheet", "character"],
+      template:
+        "systems/scion-hero-foundry/templates/actors/character-sheet.html",
+      width: 800,
+      height: 600,
+      tabs: [
+        {
+          navSelector: ".sheet-tabs",
+          contentSelector: ".sheet-body",
+          initial: "character",
+        },
+      ],
+    });
+  }
+
+  async getData() {
+    try {
+      const context = await super.getData();
+      const actorData = this.actor.toObject();
+
+      context.actor = actorData;
+      context.system = actorData.system || {};
+      context.currentUserName = game.user?.name || "";
+
+      console.log("Contexto preparado:", context);
+
+      return context;
+    } catch (e) {
+      console.error(
+        "Erro ao carregar getData ou template da ScionHeroActorSheet:",
+        e
+      );
+      return {};
+    }
+  }
+}
---
+++ module/scion-hero.js
@@ -0,0 +1,44 @@
+import { ScionHeroActorSheet } from "./actor-sheet.js";
+import { ScionHeroActorBaseDefault } from "./actor-base-default.js";
+
+Hooks.once("init", async function () {
+
+  // Remove a sheet padrão do core
+  Actors.unregisterSheet("core", "core");
+  // Registra a nova sheet como padrão para o tipo 'character'
+  Actors.registerSheet("scion-hero-foundry", ScionHeroActorSheet, {
+    types: ["character"],
+    makeDefault: true,
+    label: "Scion Hero Actor Sheet",
+  });
+
+  Handlebars.registerHelper("json", function (context) {
+    return JSON.stringify(context, null, 2);
+  });
+});
+
+Hooks.on("createActor", async (actor, options, userId) => {
+  if (actor.type !== "character") return;
+
+  const baseData = foundry.utils.mergeObject(
+    ScionHeroActorBaseDefault,
+    actor.system || {}
+  );
+  await actor.update({
+    system: baseData,
+  });
+});
+
+Hooks.on("ready", async () => {
+  for (const actor of game.actors.contents) {
+    if (actor.type === "character") {
+      const baseData = foundry.utils.mergeObject(
+        ScionHeroActorBaseDefault,
+        actor.system || {}
+      );
+      await actor.update({
+        system: baseData,
+      });
+    }
+  }
+});
