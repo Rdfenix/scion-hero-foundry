5ca2b7f7f4c7d9f2b98ddfc61177fab96b8dfc12
Rudnei Oliveira <rhoodsk8erboy@gmail.com>
2025-06-05 21:26:36 +0100

    create: creating area to select god

--- helpers/actions.js
+++ helpers/actions.js
@@ -29,11 +29,10 @@ const selectPantheon = async (actor) => {
   try {
     const pack = game.packs.get("scion-hero-foundry.deities");
     if (!pack) {
-      throw new Error("Pacote de panteões não encontrado.");
+      throw new Error("Pantheon pack not found.");
     }
 
     const deities = await pack.getDocuments();
-    console.log("Divindades encontradas no pacote:", deities);
 
     const pantheons = deities
       .map((deity) => ({
@@ -54,8 +53,6 @@ const selectPantheon = async (actor) => {
           index === self.findIndex((p) => p.name === pantheon.name)
       );
 
-    console.log("Panteões disponíveis:", pantheons);
-
     if (pantheons.length === 0) {
       throw new Error("Nenhuma divindade encontrada no pacote.");
     }
@@ -68,12 +65,12 @@ const selectPantheon = async (actor) => {
     return new Promise((resolve) => {
       new Dialog(
         {
-          title: "Selecione um Panteão",
+          title: "",
           content,
           buttons: {
             select: {
               icon: '<i class="fas fa-check"></i>',
-              label: "Selecionar",
+              label: "Select",
               class: "pantheon-select",
               callback: async (html) => {
                 const pantheonSelected = html
@@ -112,7 +109,7 @@ const selectPantheon = async (actor) => {
             },
             cancel: {
               icon: '<i class="fas fa-times"></i>',
-              label: "Cancelar",
+              label: "Cancel",
               class: "pantheon-cancel",
               callback: () => resolve(null),
             },
@@ -144,4 +141,114 @@ const selectPantheon = async (actor) => {
   }
 };
 
-const selectGod = async (actor) => {};
+const selectGod = async (actor) => {
+  try {
+    const pack = game.packs.get("scion-hero-foundry.deities");
+    if (!pack) {
+      throw new Error("Pantheon pack not found.");
+    }
+
+    const pantheon = actor.system.pantheon?.name;
+
+    const deities = await pack.getDocuments();
+
+    let gods = deities.find((p) => p.name === pantheon)?.system.deities || [];
+
+    gods = gods.map((god) => ({
+      name: god.name,
+      favoredSkills: (god.favoredSkills || []).reduce((obj, skill, idx) => {
+        obj[idx] = skill;
+        return obj;
+      }, {}),
+    }));
+
+    const content = await renderTemplate(
+      "systems/scion-hero-foundry/templates/actors/dialogs/gods.html",
+      { gods }
+    );
+
+    return new Promise((resolve) => {
+      new Dialog(
+        {
+          title: "",
+          content,
+          buttons: {
+            select: {
+              icon: '<i class="fas fa-check"></i>',
+              label: "Select",
+              class: "pantheon-select",
+              callback: async (html) => {
+                const godSelected = html
+                  .find('input[name="pantheon-option"]:checked')
+                  .val();
+
+                console.log("Deus selecionado:", godSelected);
+                if (!godSelected) {
+                  return ui.notifications.warn("Choose a god first");
+                }
+                const deityPantheon = gods.find((p) => p.name === godSelected);
+
+                if (!deityPantheon) {
+                  return ui.notifications.warn("God not found");
+                }
+
+                const abilities = actor.system.abilities;
+                const favoredSkillsArr = Object.values(
+                  deityPantheon.favoredSkills
+                );
+
+                const updatedAbilities = {};
+                for (const [key, value] of Object.entries(abilities)) {
+                  updatedAbilities[key] = {
+                    ...value,
+                    favored: favoredSkillsArr.includes(key),
+                  };
+                }
+
+                const activeTab =
+                  document.querySelector(".sheet-tabs .item.active")?.dataset
+                    .tab ?? "stats";
+
+                await actor.update({
+                  "system.abilities": updatedAbilities,
+                  "system.pantheon": {
+                    god: deityPantheon.name,
+                  },
+                });
+
+                await reopenWithActiveTab(actor, activeTab);
+              },
+            },
+            cancel: {
+              icon: '<i class="fas fa-times"></i>',
+              label: "Cancel",
+              class: "pantheon-cancel",
+              callback: () => resolve(null),
+            },
+          },
+          default: "select",
+          render: (html) => {
+            console.log("Diálogo de seleção de panteão renderizado");
+            setTimeout(() => {
+              const contentEl = html
+                .closest(".window-app")
+                .find(".window-content")[0];
+              if (contentEl) {
+                contentEl.scrollTop = 0;
+              }
+            }, 50);
+          },
+          close: () => resolve(null),
+        },
+        {
+          width: 700,
+          height: 500,
+        }
+      ).render(true);
+    });
+  } catch (error) {
+    console.error(error.message);
+    ui.notifications.error(error.message);
+    return null;
+  }
+};
