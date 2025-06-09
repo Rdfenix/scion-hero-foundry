3d17b4e485f00fb062603efb85960be6f3cb84eb
Rudnei Oliveira <rhoodsk8erboy@gmail.com>
2025-06-09 18:37:27 +0100

    create: purviews db created

--- helpers/actions.js
+++ helpers/actions.js
@@ -259,6 +259,7 @@ export async function getPurviews() {
     }
 
     const purviews = await pack.getDocuments();
+    const priorities = ["PURVIEWS", "PANTHEON–SPECIFIC PURVIEWS"];
 
     return purviews
       .map((purview) => ({
@@ -269,7 +270,21 @@ export async function getPurviews() {
       .filter(
         (purview, index, self) =>
           index === self.findIndex((p) => p.name === purview.name)
-      );
+      )
+      .sort((a, b) => {
+        const priorityA = priorities.indexOf(a.name.toUpperCase());
+        const priorityB = priorities.indexOf(b.name.toUpperCase());
+
+        if (priorityA === -1 && priorityB === -1) {
+          return a.name.localeCompare(b.name);
+        } else if (priorityA === -1) {
+          return 1; // b comes first
+        } else if (priorityB === -1) {
+          return -1; // a comes first
+        } else {
+          return priorityA - priorityB; // both are in the priorities array
+        }
+      });
   } catch (error) {
     console.error("Error fetching purviews:", error);
     ui.notifications.error("Failed to fetch purviews.");
--- helpers/journals.js
+++ helpers/journals.js
@@ -1,6 +1,6 @@
 import { getPurviews } from "./actions.js";
 
 export async function createPuviewsJournal() {
-  const result = await getPurviews();
-  console.log("Purviews fetched:", result);
+  const purviewList = await getPurviews();
+  console.log("Purviews fetched:", purviewList);
 }
